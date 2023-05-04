f2a9aedb8b2e50306668cf9d31a7350743400991
eytannnaim <80354890+eytannnaim@users.noreply.github.com>
2023-05-04 11:44:16 +0300

    DAM | Rename agent module
    [skip actions]

---
+++ iam_role.tf
@@ -0,0 +1,55 @@
+#################################
+# DSF node IAM role
+#################################
+
+locals {
+  role_arn  = aws_iam_role.dsf_node_role.arn
+  role_name = split("/", local.role_arn)[1] //arn:aws:iam::xxxxxxxxx:role/role-name
+  role_assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  inline_policy_s3 = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        "Resource" : [
+          "arn:aws:s3:::${var.binaries_location.s3_bucket}",
+          "arn:aws:s3:::${var.binaries_location.s3_bucket}/*",
+        ]
+      }
+    ]
+    }
+  )
+}
+
+resource "aws_iam_instance_profile" "dsf_node_instance_iam_profile" {
+  name_prefix = join("-", [var.friendly_name, "agent", "instance-iam-profile"])
+  role        = local.role_name
+}
+
+resource "aws_iam_role" "dsf_node_role" {
+  # name_prefix         = join("-", [var.friendly_name, "agent", "role"])
+  description         = join("-", [var.friendly_name, "agent", "role"])
+  managed_policy_arns = null
+  assume_role_policy  = local.role_assume_role_policy
+  inline_policy {
+    name   = "${var.friendly_name}-s3-access"
+    policy = local.inline_policy_s3
+  }
+}
